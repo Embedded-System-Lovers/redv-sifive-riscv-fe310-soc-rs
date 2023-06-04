7036a83af8ec762eef467638fc1499bec265db07
Gabriel Dobato <dobatog@gmail.com>
2023-06-04 13:36:44 +0200

    Replace build by check in CI until link issue is solved

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,3 @@
 pub mod panic; // Lightweight implementation of panic handler
 pub use e310x as pac; // Peripheral Access Crate. Generated from SVD file
 pub use riscv_rt::entry; // Application entry point
-#[allow(unused_imports)]
-pub use rtt_target::{rprintln as log, rtt_init_print as log_init};
